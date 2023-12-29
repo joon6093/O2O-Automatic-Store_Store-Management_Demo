d9a23d6534b7e9da99cdcfe9659f82aae1525e2a
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-12-29 18:11:46 +0900

    [INIT] 초기 환경 설정

---
+++ src/main/java/com/SJY/O2O_Automatic_Store_System_Demo/O2OAutomaticStoreSystemDemoApplication.java
@@ -0,0 +1,13 @@
+package com.SJY.O2O_Automatic_Store_System_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2OAutomaticStoreSystemDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O2OAutomaticStoreSystemDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SJY/O2O_Automatic_Store_System_Demo/O2OAutomaticStoreSystemDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SJY.O2O_Automatic_Store_System_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O2OAutomaticStoreSystemDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
