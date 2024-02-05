10b5f009a00ddaafa81de3fcd36b3542eb034964
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-02-06 00:42:03 +0900

    [FIX] 서버 개발 환경에서는 초기 데이터 추가 작업 하지 않도록 수정

--- src/main/java/com/SJY/O2O_Automatic_Store_System_Demo/InitData.java
+++ src/main/java/com/SJY/O2O_Automatic_Store_System_Demo/InitData.java
@@ -22,7 +22,7 @@ import java.util.stream.Stream;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-@Profile({"local", "dev"})
+@Profile("local")
 public class InitData {
     private final RoleRepository roleRepository;
     private final MemberRepository memberRepository;
